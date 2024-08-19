18da8e267c84857ffa30e97cbb39f87fdcd04579
Jay-9-9-9 <u22625926@tuks.co.za>
2024-08-19 18:38:23 +0200

    Defined the UnitComponent Interface , and Defined one Base class ( Infantry )

--- Infantry.h
+++ Infantry.h
@@ -1,6 +1,12 @@
 #ifndef INFANTRY_H
 #define INFANTRY_H
 
+#include "UnitComponent.h";
 
+class Infantry : public UnitComponent {
+    public :
+    void move() ;
+    void fight() ;
+};
 
 #endif
--- UnitComponent.h
+++ UnitComponent.h
@@ -1,6 +1,15 @@
 #ifndef UNITCOMPONENT_H
 #define UNITCOMPONENT_H
 
+class UnitComponent{
+    public :
+    virtual void move() = 0 ;
+    virtual void fight() = 0 ;
+    virtual void add(UnitComponent* component) = 0 ;
+    virtual void remove(UnitComponent* component) = 0 ;
+    virtual ~UnitComponent() = 0 ;
+};
+
 
 
 #endif
